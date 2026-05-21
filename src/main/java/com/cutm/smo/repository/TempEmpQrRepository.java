2832100ec4d2b1195276a15308e0cb0ee799fb38
Prem Sai <premsai200804@gmail.com>
2026-05-21 10:21:45 +0530

    Add src\main\java\com\cutm\smo\repository\TempEmpQrRepository.java: Data repository

---
+++ src/main/java/com/cutm/smo/repository/TempEmpQrRepository.java
@@ -0,0 +1,20 @@
+package com.cutm.smo.repository;
+
+import com.cutm.smo.models.TempEmpQr;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TempEmpQrRepository extends JpaRepository<TempEmpQr, Long> {
+    
+    Optional<TempEmpQr> findByQrIdAndStatus(String qrId, String status);
+    
+    List<TempEmpQr> findByStatus(String status);
+    
+    List<TempEmpQr> findByEmployeeIdAndStatus(Long employeeId, String status);
+    
+    List<TempEmpQr> findAllByOrderByCreatedAtDesc();
+}
