2eb1b8850f4f2b2aa81227f73ef97c99759c46b7
Prem Sai <premsai200804@gmail.com>
2026-05-25 23:37:34 +0530

    feat: employee data transfer object

---
+++ src/main/java/com/cutm/smo/dto/EmployeeDto.java
@@ -0,0 +1,67 @@
+package com.cutm.smo.dto;
+
+public class EmployeeDto {
+    private String empId;
+    private String empName;
+    private String roleId;
+    private String roleName;
+    private String email;
+    private String phone;
+    private String status;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
