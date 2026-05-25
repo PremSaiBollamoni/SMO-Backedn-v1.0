8db65ae03e67ba8774ce24cadc52d12edf31ee15
Prem Sai <premsai200804@gmail.com>
2026-05-25 23:41:01 +0530

    test: add Spring Boot application context test

---
+++ src/test/java/com/cutm/smo/SmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutm.smo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
