b32d09b068ee37050675356aa56e64f0cd4089fe
Prem Sai <premsai200804@gmail.com>
2026-05-25 23:40:47 +0530

    feat: vendor repository

---
+++ src/main/java/com/cutm/smo/repositories/VendorRepository.java
@@ -0,0 +1,9 @@
+package com.cutm.smo.repositories;
+
+import com.cutm.smo.models.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+}
