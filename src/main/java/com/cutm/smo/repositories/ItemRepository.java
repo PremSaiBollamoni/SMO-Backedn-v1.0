42d61aea42aeb01b13664c183427218624bf7705
Prem Sai <premsai200804@gmail.com>
2026-05-25 23:39:31 +0530

    feat: item repository

---
+++ src/main/java/com/cutm/smo/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.cutm.smo.repositories;
+
+import com.cutm.smo.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
