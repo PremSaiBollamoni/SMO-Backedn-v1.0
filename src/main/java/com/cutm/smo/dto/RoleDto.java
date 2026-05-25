cc2c8837cf6b5eda2a48f3e7ae96f2548b6b34c6
Prem Sai <premsai200804@gmail.com>
2026-05-25 23:38:21 +0530

    feat: role data transfer object

---
+++ src/main/java/com/cutm/smo/dto/RoleDto.java
@@ -0,0 +1,40 @@
+package com.cutm.smo.dto;
+
+public class RoleDto {
+    private String roleId;
+    private String roleName;
+    private String activity;
+    private String status;
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
