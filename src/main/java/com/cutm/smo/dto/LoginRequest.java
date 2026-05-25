e64d0bcca8c86c8ef617492c2e3243d5a6b78f21
Prem Sai <premsai200804@gmail.com>
2026-05-25 23:37:48 +0530

    feat: login request DTO

---
+++ src/main/java/com/cutm/smo/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.cutm.smo.dto;
+
+public class LoginRequest {
+    private String loginid;
+    private String password;
+
+    public String getLoginid() {
+        return loginid;
+    }
+
+    public void setLoginid(String loginid) {
+        this.loginid = loginid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
