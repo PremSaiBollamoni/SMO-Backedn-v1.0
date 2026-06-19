a63e3cf09c8b1575e6f1229bcde765d20d6ec376
Prem Sai Bollamoni <premsai200804@gmail.com>
2026-06-19 13:44:04 +0530

    refactor: PurchaseOrderRepository data access

--- src/main/java/com/cutm/smo/repositories/PurchaseOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cutm.smo.repositories;
-
-import com.cutm.smo.models.PurchaseOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
-}
