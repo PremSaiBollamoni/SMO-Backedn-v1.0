412e763c703024fc8d1a8b1bef22e5171cf4febe
Prem Sai Bollamoni <premsai200804@gmail.com>
2026-06-19 13:44:04 +0530

    refactor: QrEventRepository data access

--- src/main/java/com/cutm/smo/repositories/QrEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cutm.smo.repositories;
-
-import com.cutm.smo.models.QrEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QrEventRepository extends JpaRepository<QrEvent, Long> {
-}
