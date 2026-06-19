98387a7c7b21a7d22ffe34370b00cb6efe82355b
Prem Sai Bollamoni <premsai200804@gmail.com>
2026-06-19 13:44:04 +0530

    refactor: QcRepository data access

--- src/main/java/com/cutm/smo/repositories/QcRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cutm.smo.repositories;
-
-import com.cutm.smo.models.Qc;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QcRepository extends JpaRepository<Qc, Long> {
-}
