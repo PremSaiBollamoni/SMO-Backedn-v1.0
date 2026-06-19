d0ecc28e43621a79961809b57a69ec4f4da9b5ea
Prem Sai Bollamoni <premsai200804@gmail.com>
2026-06-19 13:44:03 +0530

    refactor: ItemRepository data access

--- src/main/java/com/cutm/smo/repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cutm.smo.repositories;
-
-import com.cutm.smo.models.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
